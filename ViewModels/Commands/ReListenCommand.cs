6cf75d3927fc1cb8bac9968a7d6eb65f26b64e92
Rojal Maharjan <rojma436@student.liu.se>
2022-12-19 14:19:28 +0100

    lab 1 communicating View with model

---
+++ ViewModels/Commands/ReListenCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace P2P_Chat_App.ViewModels.Commands
+{
+    internal class ReListenCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private MainViewModel _parent;
+
+        public MainViewModel Parent
+        {
+            get { return _parent; }
+            set { _parent = value; }
+        }
+
+        public ReListenCommand(MainViewModel parent)
+        {
+            this.Parent = parent;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+        public void Execute(object? parameter)
+        {
+            Parent.listen();
+        }
+    }
+}
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,15 +27,17 @@ namespace P2P_Chat_App
         {
             InitializeComponent();
             this.DataContext = mainViewModel;
+            this.Closing += mainViewModel.closingWindow;
         }
         private void TextBox_CheckNum(object sender, TextCompositionEventArgs e)
         {
             // Check if the entered text is a numerical value
-            if (!double.TryParse(e.Text, out _))
+            if (!Regex.IsMatch(e.Text, @"^\d*\.?\d*$"))
             {
                 // If the entered text is not a numerical value, cancel the event
                 e.Handled = true;
             }
         }
+
     }
 }
