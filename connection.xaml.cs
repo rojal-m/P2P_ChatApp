73d85560992dd60771c44e54c82f63c2c4b2994c
Rojal Maharjan <rojma436@student.liu.se>
2022-12-20 14:15:28 +0545

    Updated UI design

---
+++ connection.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P2P_Chat_App
+{
+    /// <summary>
+    /// Interaction logic for connection.xaml
+    /// </summary>
+    public partial class connection : Window
+    {
+        public connection()
+        {
+            InitializeComponent();
+        }
+    }
+}
